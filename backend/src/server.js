e1d8af096a14ac5c4f9227a6e4acf6f5850dfbaa
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-08-02 22:05:27 +0530

    feat(server): connect MongoDb to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(morgan("dev"));
 app.use(cors());
