7a88d752f28f307d2416721e4855d9395a466bfa
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-08-03 12:06:15 +0530

    feat(server): Add error middleware and mount it to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { errorMiddleware } from "./middleware/errorHandler.js";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +15,8 @@ connectDB();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
