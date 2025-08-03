ef46856122fb3e79b2e0fddaa2c9d42ba9a9271f
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-08-03 12:15:53 +0530

    feat(backend): Implement node Service

---
+++ backend/src/services/nodeService.js
@@ -0,0 +1,45 @@
+import Node from "../models/nodeModel.js";
+import AppError from "../utils/appError.js";
+
+export const getAllNodes = async () => {
+  return await Node.find();
+};
+
+export const addRootNode = async () => {
+  const rootNodeCount = await Node.countDocuments({ parentId: null });
+  const name = `Root ${rootNodeCount + 1}`;
+  const newNode = new Node({ name, parentId: null });
+  return await newNode.save();
+};
+
+export const addChildNode = async (parentId) => {
+  const parentNode = await Node.findById(parentId);
+  if (!parentNode) {
+    throw new AppError("Parent node not found", 404);
+  }
+
+  const parentNameParts = parentNode.name.split(" ");
+  const parentVersion = parentNameParts[parentNameParts.length - 1];
+
+  const childNodeCount = await Node.countDocuments({ parentId });
+  const newChildVersion = `${parentVersion}.${childNodeCount + 1}`;
+
+  const name = `Child ${newChildVersion}`;
+
+  const newNode = new Node({ name, parentId });
+  return await newNode.save();
+};
+
+export const updateNode = async (id, updateData) => {
+  const updatedNode = await Node.findByIdAndUpdate(
+    id,
+    { name: updateData.name },
+    { new: true }
+  );
+
+  if (!updatedNode) {
+    throw new AppError("Node not found", 404);
+  }
+
+  return updatedNode;
+};
