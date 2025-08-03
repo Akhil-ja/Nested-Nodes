a16002b5288b98d49772ea3621d4006f8fea95e3
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-08-03 12:18:39 +0530

    feat(backend): mount index routes to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import routes from "./routes/index.js";
 import { errorMiddleware } from "./middleware/errorHandler.js";
 
 dotenv.config();
@@ -15,6 +16,8 @@ connectDB();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", routes);
 
 app.use(errorMiddleware);
 
