bbffe8798b25b90c88b6811cc5fc8d5c81daf418
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-08-04 19:33:36 +0530

    fix(backend): Configure CORS using

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,12 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN,
+    methods: ["GET", "POST", "PUT", "OPTIONS"],
+  })
+);
 app.use(express.json());
 
 app.use("/api", routes);
